c5758d324893c5dd2c296a563f7c849907bf35f6
MohamedMamdouh4 <mohamedmamdouhgeeklabs@gmail.com>
2025-05-09 03:07:57 +0300

    typing updates

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
+// ********
+// on --> to listen for events
+// emit --> to send and starts events
+// ********
+
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const join = require("path").join;
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
 const app = express();
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
-const corsOptions = {
-  origin: "http://localhost:5000",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
-};
-// connectDB();
+const socketServer = createServer(app);
+const io = new Server(socketServer);
 
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("typing", (msg) => {
+    socket.broadcast.emit("typing", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+socketServer.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
 });
